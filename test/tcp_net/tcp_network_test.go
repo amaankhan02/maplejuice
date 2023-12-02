d0934f1bd824108122b502d6b57b84ec9bba5349
Amaan Khan <amaanmkhan2@gmail.com>
2023-12-02 02:06:19 -0600

    Write test cases for send file and read file - passing

--- test/tcp_net/tcp_network_test.go
+++ test/tcp_net/tcp_network_test.go
@@ -4,47 +4,29 @@ import (
 	"cs425_mp4/internal/tcp_net"
 	"cs425_mp4/internal/utils"
 	"net"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestSendFileSmall(t *testing.T) {
-	filepath := "cs425_mp4/test/test_files/tcp_net_tests/send_file_test.txt"
-	filesize := utils.GetFileSize(filepath)
+func sendFileHelper(t *testing.T, sendFilePath string) {
+	filesize := utils.GetFileSize(sendFilePath)
 
 	// dial to loopback server on port 8080
 	conn, err := net.Dial("tcp", "127.0.0.1:8080")
-	err = tcp_net.SendFile(filepath, conn, filesize)
+	if err != nil {
+		t.Errorf("Error dialing: %s", err)
+	}
+	defer conn.Close()
+
+	err = tcp_net.SendFile(sendFilePath, conn, filesize)
 	if err != nil {
 		t.Errorf("Error sending file: %s", err)
 	}
 	t.Log("File sent successfully")
 }
 
-/*
-This function is used in conjunction with TestSendFileSmall to test the
-receiving end to see if it got the file correctly
-*/
-func TestSendFileReceiver(t *testing.T) {
-
-}
-
-func TestSendFileLarge(t *testing.T) {
-
-}
-
-type TestHandler struct{}
-
-func (h *TestHandler) HandleTCPServerConnection(conn net.Conn) {
-	filepath := "cs425_mp4/test/test_files/tcp_net_tests/read_file_test.txt"
-	filesize := utils.GetFileSize(filepath)
-	err := tcp_net.ReadFile(filepath, conn, filesize)
-	if err != nil {
-		t.Errorf("Error reading file: %s", err)
-	}
-	t.Log("File read successfully")
-}
-
-func TestReadFileSmall(t *testing.T) {
+func readFileHelper(t *testing.T, saveFilepath string, fileBeingSent string) {
 	listener, err := net.Listen("tcp", ":"+"8080")
 	if err != nil {
 		t.Errorf("Error listening: %s", err)
@@ -54,8 +36,8 @@ func TestReadFileSmall(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error accepting: %s", err)
 	}
-	saveFilepath := "cs425_mp4/test/test_files/tcp_net_tests/read_file_test.txt"
-	fileBeingSent := "cs425_mp4/test/test_files/tcp_net_tests/send_file_test.txt"
+	defer conn.Close()
+
 	expectedFileSize := utils.GetFileSize(fileBeingSent)
 
 	err = tcp_net.ReadFile(saveFilepath, conn, expectedFileSize)
@@ -74,6 +56,30 @@ func TestReadFileSmall(t *testing.T) {
 		t.Errorf("Files are not equal")
 	}
 	t.Log("Files are equal")
+}
+
+func TestSendReadFile(t *testing.T) {
+	// start a goroutine to read the file first since its the server
+	var wg sync.WaitGroup
+	saveFilepath := "..\\test_files\\tcp_net_tests\\read_file_test.txt"
+	fileBeingSent := "..\\test_files\\tcp_net_tests\\send_file_test.txt"
+
+	go func() {
+		wg.Add(1)
+		readFileHelper(t, saveFilepath, fileBeingSent)
+		wg.Done()
+	}()
+	time.Sleep(500 * time.Millisecond) // wait for the server to start listening
+
+	// now send the file
+	sendFilePath := "..\\test_files\\tcp_net_tests\\send_file_test.txt"
+	sendFileHelper(t, sendFilePath)
+
+	wg.Wait() // just wait for the goroutine to finish
+}
+
+func TestSendFileLarge(t *testing.T) {
+
 }
 
 func TestReadFileLarge(t *testing.T) {
